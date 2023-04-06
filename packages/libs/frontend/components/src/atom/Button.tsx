34a9b9290686096bdc6249d833457d9461e64de3
chotaegyu <chotg@marketboro.com>
2023-04-06 10:04:13 +0900

    turborepo 로 변경

--- packages/libs/frontend/components/src/atom/Button.tsx
+++
@@ -1,9 +0,0 @@
-import {ButtonHTMLAttributes} from "react";
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-
-}
-
-export default function Button({children, ...rest}: ButtonProps) {
-    return <button {...rest}>{children}</button>
-}
